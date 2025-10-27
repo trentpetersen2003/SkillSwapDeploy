7a76afe6bcf23fea9c97d59bb9b3efc0a8f70e8e
bwolpers <bwolpers@email.sc.edu>
2025-10-26 22:08:23 -0400

    Initial setup: React client and Node server running successfully

---
+++ server/index.js
@@ -0,0 +1,6 @@
+// server/index.js
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => res.send("Server running!"));
+app.listen(3000);
+console.log("Server running on port 3000");
