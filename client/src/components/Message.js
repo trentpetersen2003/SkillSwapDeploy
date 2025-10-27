8c66851bd96b579ba88e17804606b340bb44e98a
NicolasRossetti <nicolasdrossetti@gmail.com>
2025-10-26 23:24:42 -0400

    Added components folder, and added Message.js file within folder; added Message.js functionality within App.js

---
+++ client/src/components/Message.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Message() {
+  return (
+    <div>
+      <h2>This is just an example line of text.</h2>
+      <p>Change this to something else.</p>
+    </div>
+  );
+}
+
+export default Message;
