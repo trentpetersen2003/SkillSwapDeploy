2539cb571c904afff1ae5bf5484fbd08e1db5351
NicolasRossetti <nicolasdrossetti@gmail.com>
2025-10-26 23:40:19 -0400

    Main branch: updated message text

--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -3,8 +3,8 @@ import React from "react";
 function Message() {
   return (
     <div>
-      <h2>This is just an example line of text.</h2>
-      <p>Change this to something else.</p>
+      <h2>This is just an example line of text!</h2>
+      <p>Change this to something else?!</p>
     </div>
   );
 }
